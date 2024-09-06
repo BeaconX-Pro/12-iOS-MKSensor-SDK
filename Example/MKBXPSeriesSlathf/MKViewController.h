08f9c9881cfd698846ed4df65d9fb115baa5a74a
lovexiaoxia <aadyx2007@163.com>
2024-09-06 10:51:16 +0800

    Initial commit

---
+++ Example/MKBXPSeriesSlathf/MKViewController.h
@@ -0,0 +1,13 @@
+//
+//  MKViewController.h
+//  MKBXPSeriesSlathf
+//
+//  Created by lovexiaoxia on 09/06/2024.
+//  Copyright (c) 2024 lovexiaoxia. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MKViewController : UIViewController
+
+@end
---
+++ Example/MKBXPSeriesSlathf/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MKBXPSeriesSlathf
+//
+//  Created by lovexiaoxia on 09/06/2024.
+//  Copyright (c) 2024 lovexiaoxia. All rights reserved.
+//
+
+@import UIKit;
+#import "MKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MKAppDelegate class]));
+    }
+}
